68cb9b34d44a7efa124fdb51634d066a7393b5a6
Kevin Rognon <rognonk@gmail.com>
2023-05-10 21:55:04 +0200

    Ajout du component Modal dynamique

---
+++ src/components/Modals/Modal_CV.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="bloc-modale" v-if="show">
+		<div class="overlay"></div>
+
+		<div class="modale">
+			<button @click="show != show">X</button>
+			<h3>{{ titre }}</h3>
+			<p>Technologies utlisées: {{ technos }}</p>
+			<a target="_blank" :href="lien">Voir le projet</a>
+		</div>
+	</div>
+</template>
+
+<script setup>
+
+
+defineProps({
+	titre: String,
+	technos: String,
+	lien: String,
+	show: Boolean
+})
+
+</script>
+
+<style scoped>
+
+
+
+</style>
